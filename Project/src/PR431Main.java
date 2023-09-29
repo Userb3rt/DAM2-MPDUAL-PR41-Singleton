1b8dcd17dcfcf667104bbfbfe287600bc45f44c3
userb3rt <userb3rt@github.com>
2023-09-29 18:22:50 +0200

    añadido del pdf

--- Project/src/PR431Main.java
+++ Project/src/PR431Main.java
@@ -1,11 +1,11 @@
 public class PR431Main {
     public static void main(String[] args) {
         System.out.println("Iniciant 0");
-        PR431Objecte instance1 = PR431Objecte.getInstance("Manel", "Polar", "18");
+        PR431Objecte instance1 = PR431Objecte.getInstance("Roberto", "Zambrano", "22");
         System.out.println("Iniciant 1");
-        PR431Objecte instance2 = PR431Objecte.getNewDestroyedInstance("Laura", "Gelada", "19");
+        PR431Objecte instance2 = PR431Objecte.getNewDestroyedInstance("Miguel", "Carrasco", "19");
         System.out.println("Iniciant 2");
-        PR431Objecte instance3 = PR431Objecte.getNewDestroyedInstance("Pingu", "Ice", "22");
+        PR431Objecte instance3 = PR431Objecte.getNewDestroyedInstance("Ahmed", "Jalil", "11");
         System.out.println(instance1.toString());
         System.out.println(instance2.toString());
         System.out.println(instance3.toString());
